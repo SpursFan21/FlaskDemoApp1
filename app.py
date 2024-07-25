e6607dc7bee572bfe61623aa6c5edca44080aa94
SpursFan21 <duncanfrederic@gmail.com>
2024-07-26 10:26:06 +1200

    update db in app

--- app.py
+++ app.py
@@ -2,12 +2,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import Flask,render_template,request, redirect
 from models import db, InfoModel
+import os
 
 from flask import Flask
  
 app = Flask(__name__)
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
+# our database uri
+if 'RDS_DB_NAME' in os.environ:
+    app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+        username=os.environ['RDS_USERNAME'],
+        password=os.environ['RDS_PASSWORD'],
+        host=os.environ['RDS_HOSTNAME'],
+        port=os.environ['RDS_PORT'],
+        database=os.environ['RDS_DB_NAME'],
+    )
+else:
+    # our database uri
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
+ 
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Cellfone01@localhost:5432/flaskdemoappdb"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
  
